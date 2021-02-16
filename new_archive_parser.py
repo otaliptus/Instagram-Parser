ec3ef95fc20c351b4618610755353393420d7e9d
omertalib42 <omertalib42@gmail.com>
2021-02-16 10:29:57 +0300

    Added new parser

---
+++ new_archive_parser.py
@@ -0,0 +1,27 @@
+import simplejson as json
+import os
+
+dirname = os.path.dirname(__file__)
+
+
+def combiner(current_path, file):
+    print(current_path, file)
+    with open(os.path.join(current_path, file), 'r', encoding='utf-8') as input_file:
+        text = json.load(input_file)
+        # TODO: Fix facebook user conflict.
+        username = current_path[2:-11]
+        with open(os.path.join(current_path, 'SIMPLIFIED_' + username + '.json'), 'a', encoding='utf-8') as output_file:
+            output_file.write(json.dumps(text, indent=2, sort_keys=True, ensure_ascii=False))
+
+
+def traveler():
+    os.chdir('messages/inbox')
+    for current_path, folders, files in os.walk('.'):
+        for file in files:
+            name, ext = os.path.splitext(file)
+            # TODO: Add regex check for file name.
+            if name.startswith('message') and ext == '.json':
+                combiner(current_path, file)
+
+
+traveler()
