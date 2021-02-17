8fb6aaa3a53a240a3b8dd02648a2b65b05130fe0
omertalib42 <omertalib42@gmail.com>
2021-02-17 11:04:55 +0300

    Fixed facebookuser issue

--- new_archive_parser.py
+++ new_archive_parser.py
@@ -2,14 +2,18 @@ import simplejson as json
 import os
 
 dirname = os.path.dirname(__file__)
+facebook_cnt = 1
 
 
 def combiner(current_path, file):
+    global facebook_cnt
     # TODO: Fix unicode.
     with open(os.path.join(current_path, file), 'r', encoding='utf-8') as input_file:
         text = json.load(input_file)
-        # TODO: Fix facebook user conflict.
         username = current_path[2:-11]
+        if username == 'facebookuser':
+            username = username + str('_{}'.format(facebook_cnt))
+            facebook_cnt += 1
         with open(os.path.join(current_path, 'SIMPLIFIED_' + username + '.json'), 'a', encoding='utf-8') as output_file:
             output_file.write(json.dumps(text, indent=2, sort_keys=True, ensure_ascii=False))
 
